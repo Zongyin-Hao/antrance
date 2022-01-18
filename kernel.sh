b635d115d60f5289bb4aeb39a6cedbb349e4d8c3
Zongyin-Hao <haozongyin@gmail.com>
2022-01-18 19:29:46 +0800

    change workspace(hzy-PC)

--- kernel.sh
+++ kernel.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-database=/home/android/project/xmu_wrxlab/auiauto/database
+database=/home/hzy/hzy/projects/android/auiauto/database
 mkdir -p ${database}/kernel/
 cp app/build/outputs/apk/debug/app-debug.apk ${database}/kernel/antrance.apk
 cp -r app/build/intermediates/javac/debug/classes/*.class ${database}/kernel/
