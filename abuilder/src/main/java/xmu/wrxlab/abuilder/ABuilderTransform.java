1f38bb6796d0dc2da741fc3b2352f191bfbf8a96
Zongyin-Hao <haozongyin@gmail.com>
2022-04-18 21:52:10 +0800

    修改command.sh路径

--- abuilder/src/main/java/xmu/wrxlab/abuilder/ABuilderTransform.java
+++ abuilder/src/main/java/xmu/wrxlab/abuilder/ABuilderTransform.java
@@ -293,7 +293,7 @@ public class ABuilderTransform extends Transform {
                     }
                 }
 
-                UzipTojar.toJar(myConfig.getDatabase() + "/command.sh"
+                UzipTojar.toJar(myConfig.getDatabase() + "/kernel/command.sh"
                         , jarOutput.getAbsolutePath(), dst.getAbsolutePath());
 
                 // 更新sootId
